f9d58c95440b293470b8493ded84085c8da51a2a
Gautam Amin <gautam.a@cheq.ai>
2025-01-16 17:01:01 -0600

    release 1.0.0

--- Sources/Cheq/Info.swift
+++ Sources/Cheq/Info.swift
@@ -4,7 +4,7 @@ import UIKit
 enum Info {
     
     static let library_name = "cheq-sst-swift"
-    static let library_version = "0.4.0"
+    static let library_version = "1.0.0"
     
     static var cpuArchitecture: String = {
 #if arch(x86_64)
